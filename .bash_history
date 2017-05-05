82a3f0f60c5c75666ab783c7733d5721ef4f6d2a
Sevki Yontem <sevkiyontem@gmail.com>
2017-05-05 15:08:12 +0300

    Proper Old input now sticks to input field after an error

--- .bash_history
+++ .bash_history
@@ -1,3 +1,27 @@
 pwd
 pwd
 pwd
+pwd
+git status
+git stash
+git status
+git stash clear
+git status
+git stash
+git status
+git stash show
+git stash
+git config --global user.email "sevkiyontem@gmail.com"
+git config --global user.name "sevkibaba"
+git stash
+git status
+git stash show
+git pull origin master
+git stash apply
+git add .
+git status
+git remove .
+git status
+git commit -m "cleared some code and started data table for check page"
+git push origin master
+git status
--- app/Middleware/OldInputMiddleware.php
+++ app/Middleware/OldInputMiddleware.php
@@ -6,17 +6,21 @@ class OldInputMiddleware extends Middleware {
 
 	public function __invoke ($request, $response, $next){
 
-		if (isset($_SESSION['old'])){
+		if (!isset($_SESSION['old'])){
 
-		$this->container->view->getEnvironment()->addGlobal('old', $_SESSION['old']);
-		$_SESSION['old']= $request->getParams();
+			$this->container->view->getEnvironment()->addGlobal('old', $_SESSION['old']);
+			$_SESSION['old'] = array();
 		
+		}else{
+			$this->container->view->getEnvironment()->addGlobal('old', $_SESSION['old']);
+			$_SESSION['old']= $request->getParams();
 		}
 
 		$response = $next($request, $response);
 
+		// var_dump( $_SESSION['old']);
+		// die();
 		return $response;
-
 	}
 
 }
